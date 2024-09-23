7d6007505d75593eb5782adb46d3a68627fd73b9
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-23 08:51:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Topic_7_Assignment___Lists_Assignment
+{
+    //Christian
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int chooseVal;
+            int menuChoice;
+            int addUp;
+            bool done = false;
+
+
+            Random generator = new Random();
+            List<int> numbers = new List<int>();
+
+
+
+
+
+            Console.WriteLine("Hello, World!");
+
+
+
+
+        }
+    }
+}
