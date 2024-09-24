30d780482cca6b4250574758c63ce79123bb1d6b
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-24 09:37:25 -0400

    Done Part 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Topic_7_Assignment___Lists_Assignment
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Topic_7_Assignment___Lists_Assignment
 {
     //Christian
     internal class Program
@@ -9,9 +11,10 @@
 
             int chooseVal;
             int menuChoice;
-            int addUp, average;
             int smallest, largest;
+            int count;
             int listLength;
+            double addUp, average;
             bool done = false;
 
 
@@ -44,9 +47,9 @@
                 Console.WriteLine("3 - Remove a number (by value - all instances)");
                 Console.WriteLine("4 - Add a value to the list");
                 Console.WriteLine("5 - Count the number of occurrences of a specified value");
-                Console.WriteLine("6 - Print the largest number");
-                Console.WriteLine("7 - Print the smallest number");
-                Console.WriteLine("8 - Print the sum and average of all the numbers");
+                Console.WriteLine("6 - Find the largest number");
+                Console.WriteLine("7 - Find the smallest number");
+                Console.WriteLine("8 - Find the sum and average of all the numbers");
                 Console.WriteLine("9 - Quit (And move on to part 2)");
 
                 while (!Int32.TryParse(Console.ReadLine().Trim(), out menuChoice) || menuChoice <= 0 || menuChoice > 9)
@@ -82,63 +85,134 @@
 
                     while (!Int32.TryParse(Console.ReadLine().Trim(), out chooseVal))
                     {
-                        Console.WriteLine("That is not a valid input, please input an integer you wish to remove from the list.");
+                        Console.WriteLine("That is not a valid input. Please input an integer you wish to remove from the list.");
                     }
 
                     if (chooseVal < 10 || chooseVal > 20)
                     {
                         Console.WriteLine("That value is outside the possible range of the list, and as such, there are no instances to remove.");
+                        Console.WriteLine();
                     }
                     else
                     {
                         if (numbers.Contains(chooseVal))
                         {
                             numbers.RemoveAll(x => x == chooseVal);
+
+                            Console.WriteLine($"All instances of {chooseVal} have been removed.");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"There are no instances of {chooseVal} in the list");
+                            Console.WriteLine();
                         }
                     }
-
-
-
-
                 }
                 else if (menuChoice == 4)
                 {
+                    Console.WriteLine("Please input a value between 10-20 to add to the end of the list:");
 
+                    while (!Int32.TryParse(Console.ReadLine(), out chooseVal) || chooseVal < 10 || chooseVal > 20)
+                    {
+                        Console.WriteLine("That is not a valid input. Please input an integer between 10-20 to add to the list.");
+                    }
+
+                    numbers.Add(chooseVal);
+
+                    Console.WriteLine($"{chooseVal} has been added to the end of the list");
+                    Console.WriteLine();
                 }
                 else if (menuChoice == 5)
                 {
+                    count = 0;
 
+                    Console.WriteLine();
+                    Console.WriteLine("What value would you like to count?");
+
+                    while (!Int32.TryParse(Console.ReadLine().Trim(), out chooseVal))
+                    {
+                        Console.WriteLine("That is not a valid input. Please input a valid integer.");
+                    }
+
+                    if (chooseVal < 10 || chooseVal > 20)
+                    {
+                        Console.WriteLine($"{chooseVal} is outside the possible range of the list, and thus there are {count} instances of it");
+                        Console.WriteLine();
+                    }
+                    else 
+                    {
+                        foreach (int num in numbers)
+                        {
+                            if (num == chooseVal)
+                            {
+                                count++;
+                            }
+                        }
+                        Console.WriteLine($"There are {count} instances of {chooseVal} in the list");
+                        Console.WriteLine();
+                    }
                 }
                 else if (menuChoice == 6)
                 {
+                    largest = 10;
 
+                    foreach (int num in numbers)
+                    {
+                        if (num > largest)
+                        {
+                            largest = num;
+                        }
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine($"The largest value in the list is {largest}");
+                    Console.WriteLine();
                 }
                 else if (menuChoice == 7)
                 {
+                    smallest = 20;
 
+                    foreach (int num in numbers)
+                    {
+                        if (num < smallest)
+                        {
+                            smallest = num;
+                        }
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine($"The smallest value in the list is {smallest}");
+                    Console.WriteLine();
                 }
                 else if (menuChoice == 8)
                 {
+                    addUp = 0;
+                    average = 0;
 
+                    foreach (int num in numbers)
+                    {
+                        addUp += num;
+                    }
+
+                    average = addUp / numbers.Count;
+
+                    Console.WriteLine();
+                    Console.WriteLine($"The sum of all the values in the list is {addUp}, and the average is {average}.");
+                    Console.WriteLine();
                 }
                 else if (menuChoice == 9)
                 {
+                    done = true;
 
+                    Console.WriteLine();
+                    Console.WriteLine("Okay!");
+                    Console.WriteLine();
                 }
-
-
-
-
-
-
                 Console.WriteLine("Press ENTER to continue");
                 Console.ReadLine();
                 Console.Clear();
             }
-
-
-
-
         }
     }
 }
